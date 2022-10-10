f76462732e4223a2cc00da4dc9a2e349e9b3f73e
Jose Saint Hilaire <itt.jose.sainthilaire@gmail.com>
2022-10-10 09:56:53 -0400

    add totalSupply and balance

--- src/contracts/erc20.ts
+++ src/contracts/erc20.ts
@@ -1,4 +1,4 @@
-import { BaseContract, utils, BigNumber } from 'ethers';
+import { BaseContract, utils, BigNumber, BigNumberish } from 'ethers';
 import { ContractCore } from './contractCore';
 
 export class Erc20<T extends BaseContract> {
@@ -9,6 +9,8 @@ export class Erc20<T extends BaseContract> {
   }
 
   // TODO: validate the contract capabilities before sending a transaction
+  // TODO: add currency custom type to be returned on the methods that needs it
+  // TODO: fetch decimals and other metadata for return a more a accurate value
   public async mint(to: string, amount: string): Promise<any> {
     return {
       receipt: await this.contractCore.sendTransaction('mint', [
@@ -27,9 +29,23 @@ export class Erc20<T extends BaseContract> {
     };
   }
 
+  public async balanceOf(address: string): Promise<any> {
+    return await this.contractCore.readonlyContract.balanceOf(address);
+  }
+
+  public async totalSupply(): Promise<any> {
+    const totalSupply = await this.contractCore.readonlyContract.totalSupply();
+    return await this.formatAmount(totalSupply);
+  }
+
   public async normalizeAmount(amount: string): Promise<BigNumber> {
     // TODO: add a new type with validation for the amount
     const decimals = await this.contractCore.readonlyContract.decimals();
     return utils.parseUnits(amount, decimals);
+  }
+
+  public async formatAmount(amount: BigNumberish): Promise<string> {
+    const decimals = await this.contractCore.readonlyContract.decimals();
+    return utils.formatUnits(amount, decimals);
   }
 }
