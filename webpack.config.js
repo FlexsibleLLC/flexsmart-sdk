32f880104e6cf8427022783f6d967e6dda07386c
Dariel de Jesus <darieldejesus@gmail.com>
2023-08-14 09:25:00 -0400

    Setup minifier to the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -10,10 +11,14 @@ module.exports = {
   output: {
     library: "FlexSmart",
     path: path.resolve(__dirname, './dist_bundle'),
-    filename: "bundle.js"
+    filename: "bundle.min.js"
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   plugins: [
     new webpack.DefinePlugin({
